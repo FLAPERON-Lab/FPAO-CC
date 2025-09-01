afaad1e63d9273d22de3963d85d2483a188698f6
federicoangioni <f.angioni@student.tudelft.nl>
2025-09-01 18:17:07 +0200

    Add visualisations in aerodynamic efficiency

--- notebooks/AerodynamicEfficiency.py
+++ notebooks/AerodynamicEfficiency.py
@@ -1,25 +1,39 @@
 import marimo
 
-__generated_with = "0.13.4"
+__generated_with = "0.15.0"
 app = marimo.App(width="medium")
 
 with app.setup:
     # Initialization code that runs before all other cells
     import marimo as mo
-    from core import _defaults
 
+    # Import dependencies
+    from core import _defaults
+    from plotly.subplots import make_subplots
+    import plotly.graph_objects as go
+    import plotly.express as px
+    import numpy as np
+    from core import aircraft as ac
+
+
+    # Set local/online filepath
     _defaults.FILEURL = _defaults.get_url()
 
+    # Plotly dark mode template
     _defaults.set_plotly_template()
+
+    # Data directory
+    data_dir = str(mo.notebook_location() / "public" / "AircraftDB_Standard.csv")
 
 
 @app.cell
 def _():
+    # Set navbar on the right
     _defaults.set_sidebar()
     return
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _():
     mo.md(
         r"""
@@ -39,7 +53,7 @@ def _():
     return
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _():
     mo.callout(
         mo.md(
@@ -54,7 +68,7 @@ def _():
     return
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _():
     mo.md(
         r"""
@@ -66,10 +80,6 @@ def _():
     $$C_L=C_{L_\mathrm{max}} \quad \Rightarrow \quad  E_S = \frac{C_{L_\mathrm{max}}}{C_{D_0}+K C_{L_\mathrm{max}}}$$
 
     E is a non monotonic function of the $C_L$, and therefore it is expected that it will have some staionary points inside the domain.
-
-    - [ ] Plot CD vs CL, E vs CL.
-    - [ ] Indicate with S the stall point and with E the point corresponding the Emax.
-    - [ ] On the CD vs CL chart, show the tangent line corresponding to max E
     """
     )
     return
@@ -77,11 +87,221 @@ def _():
 
 @app.cell
 def _():
+    data = ac.available_aircrafts(data_dir).loc[
+        :,
+        [
+            "full_name",
+            "ID",
+            "type",
+            "b",
+            "S",
+            "CD0",
+            "K",
+            "CLmax_ld",
+            "MTOM",
+            "OEM",
+        ],
+    ]
+    return (data,)
+
+
+@app.cell(hide_code=True)
+def _(data):
+    # Database cell (1)
+
+
+    ac_table = mo.ui.table(
+        data=data,
+        pagination=True,
+        show_column_summaries=False,
+        selection="single",
+        initial_selection=[0],
+        page_size=4,
+        show_data_types=False,
+    )
+
+    ac_table
+    return (ac_table,)
+
+
+@app.cell
+def _(ac_table, data):
+    # Variables declared
+    meshgrid_n = 101
+    xy_lowerbound = -0.1
+
+    CL_buffer = 1
+    # Handle deselected row from table
+    if ac_table.value is not None and ac_table.value.any().any():
+        active_selection = ac_table.value.iloc[0]
+    else:
+        active_selection = data.iloc[0]
+
+
+    CLmax = active_selection["CLmax_ld"]
+    CD0 = active_selection["CD0"]
+    K = active_selection["K"]
+
+    CL_array = np.linspace(0, CLmax + CL_buffer, meshgrid_n)
+    CD_array = CD0 + K * CL_array**2
+    E_array = CL_array / (CD0 + K * CL_array**2)
+    CL_E = np.sqrt(CD0 / K)
+    CD_E = CD0 + K * CL_E**2
+    E_max = CL_E / (CD0 + K * CL_E**2)
+    E_max_line = CD_E / CL_E * CL_array
+    return (
+        CD_E,
+        CD_array,
+        CL_E,
+        CL_array,
+        CLmax,
+        E_array,
+        E_max,
+        E_max_line,
+        active_selection,
+    )
+
+
+@app.cell
+def _(
+    CD_E,
+    CD_array,
+    CL_E,
+    CL_array,
+    CLmax,
+    E_array,
+    E_max,
+    E_max_line,
+    active_selection,
+):
+    fig_endurance = make_subplots(
+        rows=1, cols=2, specs=[[{"type": "xy"}, {"type": "xy"}]]
+    )
+
+
+    fig_endurance.add_traces(
+        [
+            go.Scatter(
+                x=CL_array,
+                y=CD_array,
+                showlegend=True,
+                name=r"$C_D$",
+                line=dict(color="rgba(0, 114, 178, 1)"),
+            ),
+            go.Scatter(
+                x=CL_array,
+                y=E_max_line,
+                showlegend=False,
+                line=dict(color="rgba(230, 159, 0, 1)", dash="dot"),
+            ),
+            go.Scatter(
+                x=[CL_E],
+                y=[CD_E],
+                line=dict(color="rgba(240, 228, 66, 1)"),
+                mode="markers+text",
+                text=[r"${E_{\mathrm{max}}}$"],
+                showlegend=False,
+                textposition="top left",
+            ),
+        ],
+        cols=1,
+        rows=1,
+    )
+
+    fig_endurance.add_traces(
+        [
+            go.Scatter(x=CL_array, y=E_array, name=r"$E$"),
+            go.Scatter(
+                x=[CL_E],
+                y=[E_max],
+                name=r"$E_{\mathrm{max}}$",
+                marker=dict(color="rgba(240, 228, 66, 1)"),
+                text=[r"${E_{\mathrm{max}}}$"],
+                mode="markers+text",
+                textposition="top left",
+            ),
+        ],
+        cols=2,
+        rows=1,
+    )
+
+    fig_endurance.add_vline(
+        x=CLmax,
+        line_dash="dot",
+        line_color="red",
+        annotation=dict(text=r"$C_{L_{\mathrm{max}}}$", xshift=10, yshift=-10),
+        row=1,
+        col=1,
+    )
+
+    fig_endurance.add_shape(
+        type="line",
+        x0=CL_E,
+        x1=CL_E,
+        y0=0,
+        y1=CD_E,
+        line_dash="dot",
+        line_color="rgba(0, 158, 115, 1)",
+        row=1,
+        col=1,
+    )
+
+    fig_endurance.add_shape(
+        type="line",
+        x0=CL_E,
+        x1=CL_E,
+        y0=0,
+        y1=E_max,
+        line_dash="dot",
+        line_color="rgba(0, 158, 115, 1)",
+        row=1,
+        col=2,
+    )
+
+    fig_endurance.add_annotation(
+        x=CL_E - 0.1, y=0, text=r"$C_{L_E}$", showarrow=False, col=1, row=1
+    )
+    fig_endurance.add_annotation(
+        x=CL_E - 0.1, y=0, text=r"$C_{L_E}$", showarrow=False, col=2, row=1
+    )
+
+
+    fig_endurance.add_annotation(
+        x=CL_array[-10],
+        y=E_max_line[-10] + 0.09,
+        text=r"$\left(\frac{C_D}{C_L}\right)_{\mathrm{max}}$",
+        showarrow=False,
+        col=1,
+        row=1,
+    )
+
+
+    fig_endurance.update_xaxes(title_text=r"$C_L \; (-)$", col=1, row=1)
+    fig_endurance.update_yaxes(title_text=r"$C_D \; (-)$", col=1, row=1)
+    fig_endurance.update_xaxes(title_text=r"$C_L \; (-)$", col=2, row=1)
+    fig_endurance.update_yaxes(title_text=r"$E \; (-)$", col=2, row=1)
+
+    fig_endurance.update_layout(
+        title_text=active_selection["full_name"],
+        title_x=0.5,
+    )
+    mo.output.clear()
+    return (fig_endurance,)
+
+
+@app.cell
+def _(fig_endurance):
+    fig_endurance
+    return
+
+
+@app.cell(hide_code=True)
+def _():
     mo.md(r"""## Optimization""")
     return
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _():
     mo.md(r"""
     Find the maximum aerodynamic efficiency by changing the lift coefficient within certain limits
@@ -102,7 +322,7 @@ def _():
     return
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _():
     mo.md(
         r"""
@@ -115,7 +335,7 @@ def _():
     return
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _():
     mo.md(
         r"""
@@ -126,7 +346,7 @@ def _():
     return
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _():
     mo.md(
         r"""
@@ -137,7 +357,7 @@ def _():
     return
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _():
     mo.md(
         r"""
