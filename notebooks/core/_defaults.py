39c8c10b8594df76d2557b20685f6f6cf2f8a904
federicoangioni <f.angioni@student.tudelft.nl>
2025-09-18 23:13:05 +0200

    Add MaxAltitude homepage and add it to the sidebar

--- notebooks/core/_defaults.py
+++ notebooks/core/_defaults.py
@@ -48,6 +48,7 @@ def set_sidebar():
                     f"{FILEURL}AerodynamicEfficiency.py": "Aerodynamic Efficiency",
                     f"{FILEURL}MinSpeed.py": "Minimum Speed",
                     f"{FILEURL}MinPower.py": "Minimum Power",
+                    f"{FILEURL}MaxAltitude.py": "Maximum Altitude",
                 },
             },
             orientation="vertical",
