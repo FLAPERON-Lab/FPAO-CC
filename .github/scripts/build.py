d9e2cc0e49574060025892c5a06313646a5dcbef
federicoangioni <f.angioni@student.tudelft.nl>
2025-09-20 13:02:45 +0200

    Fix typo in build.py

--- .github/scripts/build.py
+++ .github/scripts/build.py
@@ -29,7 +29,8 @@ import fire
 
 from loguru import logger
 
-githube_repo_name "FPAO-CC"
+github_repo_name = "FPAO-CC"
+
 
 def _adapt_to_wasm(notebook_path: Path, output_dir: Path):
     block_to_insert = """    # For online support with WASM and Pyodide ===================
@@ -60,7 +61,7 @@ def _adapt_to_wasm(notebook_path: Path, output_dir: Path):
 
         for line in lines:
             modified_line = line.replace(".py", ".html")
-            modified_line = line.replace("/?file=", f"/{githube_repo_name}/notebooks")
+            modified_line = line.replace("/?file=", f"/{github_repo_name}/notebooks")
             new_lines.append(modified_line)
             if "import marimo as mo" in line:
                 new_lines.append(block_to_insert)
