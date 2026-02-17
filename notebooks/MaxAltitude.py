2d0f29375d8663010610655592d863a04a5023c9
federicoangioni <f.angioni@student.tudelft.nl>
2026-02-17 10:00:02 +0100

    Fix importing issues with main steady level flight notebooks

--- notebooks/MaxAltitude.py
+++ notebooks/MaxAltitude.py
@@ -6,24 +6,16 @@ app = marimo.App(width="medium")
 with app.setup:
     # Initialization code that runs before all other cells
     import marimo as mo
-
-    # Import dependencies
     from core import _defaults
-    from plotly.subplots import make_subplots
     import plotly.graph_objects as go
+    from plotly.subplots import make_subplots
     import plotly.express as px
     import numpy as np
-    from core import atmos
     from core import aircraft as ac
-    from core.aircraft import velocity, horizontal_constraint, power, drag
 
-    # Set local/online filepath
     _defaults.FILEURL = _defaults.get_url()
 
-    # Plotly dark mode template
     _defaults.set_plotly_template()
-
-    # Data directory
     data_dir = str(mo.notebook_location() / "public" / "AircraftDB_Standard.csv")
 
 
